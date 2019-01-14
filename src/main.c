fde3025d8871bb4949b818109d64f428868960dd
evdrvn <evdrvn@gmail.com>
2019-01-14 14:52:12 +0000

    move control modified

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <limits.h>
 #include <string.h>
+#include <signal.h>
 #include <wiringPi.h>
 #include <evdsptc.h>
 #include <drv8830-i2c.h>
@@ -11,10 +12,10 @@
 #define DISTANCE_PER_COUNT (215 / 40.0F) // mm
 #define LOG_SIZE (1024)
 #define PERIODS_SPEED_AVERAGE (16)
-#define DRIVE_VOLTAGE (2.00F)
-#define DECEL_VOLTAGE (1.65F)
-#define DECEL_START (25.0F)
-#define STOP_START (10.0F)
+#define DRIVE_VOLTAGE (3.00F)
+#define DECEL_VOLTAGE (1.80F)
+#define STOP_START (DISTANCE_PER_COUNT * 2.0F)
+#define DECEL_START (STOP_START * 3.0F)
 #define MOTOR_NUM (2)
 #define I2C_DEVNAME "/dev/i2c-1"
 #define MAX_WS_CLIENTS (2)
@@ -42,9 +43,6 @@ typedef struct posctrl_context {
     unsigned int period;
     int count;
     float position;
-    float target_position;
-    int state;
-    int coin_count;
     float speed; 
     float voltage;
     float voltage_offset;
@@ -57,6 +55,9 @@ typedef struct periodic_context {
     long long int min;
     struct timespec prev;
     bool initial;
+    float move_remaining;
+    t_drive_status state;
+    int coin_count;
     t_posctrl_context posctx[2];
     pthread_mutex_t mutex;
 } t_periodic_context;
@@ -68,17 +69,14 @@ typedef struct ws_client {
 
 static struct periodic_context prdctx;
 static t_ws_client ws_clients[MAX_WS_CLIENTS];
-static volatile bool finalize = false;
+static volatile sig_atomic_t finalize = 0;
 
 static long long int timespec_diff(struct timespec *t1, struct timespec *t2){
     return  t2->tv_nsec - t1->tv_nsec + (t2->tv_sec - t1->tv_sec) * NS_AS_SEC;
 }
 
 static void move_forward_to(t_periodic_context* prdctx, float distance){
-    int i;
-    for(i = 0; i < 2; i++){
-        prdctx->posctx[i].target_position = prdctx->posctx[i].position + distance; 
-    }
+    prdctx->move_remaining += distance * 2.0F; 
 }
 
 static void turn(t_periodic_context* prdctx, float voltage_offset){
@@ -91,7 +89,7 @@ static bool periodic_routine(evdsptc_event_t* event){
     t_periodic_context* prdctx = (struct periodic_context*)evdsptc_event_getparam(event);
     long long int interval = TICK_NS;
     int i, input;
-    float remaining;
+    float v;
 
     clock_gettime(CLOCK_REALTIME, &now);
     if(prdctx->initial) prdctx->initial = false;
@@ -105,31 +103,32 @@ static bool periodic_routine(evdsptc_event_t* event){
             prdctx->posctx[i].last_input = input; 
             prdctx->posctx[i].count++;
             prdctx->posctx[i].position = prdctx->posctx[i].count * DISTANCE_PER_COUNT;
+            if(prdctx->move_remaining > 0.0F) prdctx->move_remaining -= DISTANCE_PER_COUNT;
         }
         prdctx->posctx[i].speed = 
             (prdctx->posctx[i].position - prdctx->posctx[i].log[(prdctx->posctx[i].period - PERIODS_SPEED_AVERAGE) % LOG_SIZE].position) / 
             (PERIODS_SPEED_AVERAGE * TICK_SEC);
+    }   
+    
+    if(prdctx->move_remaining > STOP_START * 2.0F) prdctx->state = STATE_DRIVE;
+    if(prdctx->state > STATE_COAST && prdctx->state < STATE_DECEL && prdctx->move_remaining < DECEL_START * 2.0F) prdctx->state = STATE_DECEL;
+    if(prdctx->state > STATE_COAST && prdctx->state < STATE_STOP && prdctx->move_remaining < STOP_START * 2.0F){
+        prdctx->state = STATE_STOP;
+        prdctx->coin_count = 5;
+    }
+    if(prdctx->state == STATE_STOP && prdctx->coin_count-- <= 0) prdctx->state = STATE_COAST;
+    
+    for(i = 0; i < 2; i++){
 
-        remaining = prdctx->posctx[i].target_position - prdctx->posctx[i].position;
-        if(remaining > STOP_START) prdctx->posctx[i].state = STATE_DRIVE;
-        if(prdctx->posctx[i].state > STATE_COAST && prdctx->posctx[i].state < STATE_DECEL && remaining < DECEL_START) prdctx->posctx[i].state = STATE_DECEL;
-        if(prdctx->posctx[i].state > STATE_COAST && prdctx->posctx[i].state < STATE_STOP && remaining < STOP_START){
-            prdctx->posctx[i].state = STATE_STOP;
-            prdctx->posctx[i].coin_count = 5;
-        }
-
-        if(prdctx->posctx[i].state == STATE_DRIVE) prdctx->posctx[i].voltage = DRIVE_VOLTAGE;
-        else if(prdctx->posctx[i].state == STATE_DECEL) prdctx->posctx[i].voltage = DECEL_VOLTAGE;
-        else if(prdctx->posctx[i].state == STATE_STOP){
+        if(prdctx->state == STATE_DRIVE) prdctx->posctx[i].voltage = DRIVE_VOLTAGE;
+        else if(prdctx->state == STATE_DECEL) prdctx->posctx[i].voltage = DECEL_VOLTAGE;
+        else if(prdctx->state == STATE_STOP){
             prdctx->posctx[i].voltage = 0.0F;
-            if(prdctx->posctx[i].coin_count-- < 0) prdctx->posctx[i].state = STATE_COAST;
         }
 
-        for(int i = 0; i < MOTOR_NUM; i++){
-            prdctx->posctx[i].voltage += prdctx->posctx[i].voltage_offset;
-            prdctx->posctx[i].voltage *= DIRECTION_CORRECTION[i];
-        }
-        drv8830_move(&prdctx->posctx[i].conn, prdctx->posctx[i].voltage);
+        if(prdctx->state > STATE_STOP) v = (prdctx->posctx[i].voltage + prdctx->posctx[i].voltage_offset) * DIRECTION_CORRECTION[i];
+        else v = prdctx->posctx[i].voltage;
+        drv8830_move(&prdctx->posctx[i].conn, v);
 
         prdctx->posctx[i].log[prdctx->posctx[i].period % LOG_SIZE].voltage = prdctx->posctx[i].voltage;
         prdctx->posctx[i].log[prdctx->posctx[i].period % LOG_SIZE].position = prdctx->posctx[i].position;
@@ -137,7 +136,7 @@ static bool periodic_routine(evdsptc_event_t* event){
 
         prdctx->posctx[i].period++;
     }
-    return finalize;
+    return (bool)finalize;
 }
 
 static void createresponse(char* buf, unsigned int size){
@@ -204,20 +203,26 @@ static int WebsocketDataHandler(struct mg_connection *conn, int bits, char *data
     char buf[BUFSIZ]; 
     char *cmd = NULL;
     char *c;
+    char *val;
+    float v;
 
     if(len >= BUFSIZ) len = BUFSIZ - 1;
     memcpy(buf, data, len);
     buf[len] = '\0';
-    printf("%s\n", buf);
+    printf("%s", buf);
     
     cmd = strtok_r(buf, ":", &c);
+    val = strtok_r(NULL, ",", &c);
+    v = atof(val);
 
     if(0 == strncmp("move_to", cmd, 6)) {
-        move_forward_to(&prdctx, 0.0F);
+        printf(" -> val = %f\n", v);
+        move_forward_to(&prdctx, v * STOP_START * 3.0F * 2.0F);
     }
     else if(0 == strncmp("turn", cmd, 4)) {
-        turn(&prdctx, 0.0F);
-    }
+        printf(" -> val = %f\n", v);
+        turn(&prdctx, v * 0.6F);
+    }else printf("\n");
     return 1;
 }
 
@@ -231,6 +236,10 @@ static void WebSocketCloseHandler(const struct mg_connection *conn, void *cbdata
     client->state = 0;
     client->conn = NULL;
     mg_unlock_context(ctx);
+}
+
+void signal_handler(int signum) {
+    finalize = 1;
 }
 
 int main(void){
@@ -274,6 +283,9 @@ int main(void){
         if(0 > drv8830_open(&prdctx.posctx[i].conn, I2C_DEVNAME, I2C_ADDRESS[i], 2)) return 2; 
     }
 
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+
     evdsptc_setmutexattrinitializer(&mutexattr);
     evdsptc_create_periodic(&ctx, NULL, NULL, NULL, &interval);
 
@@ -299,6 +311,7 @@ int main(void){
     evdsptc_destroy(&ctx, true);
 
     for(j = 0; i < MOTOR_NUM; j++){
+        drv8830_move(&prdctx.posctx[i].conn, 0.0F);
         for(i = 0; i < LOG_SIZE; i++){
             printf("%f, %f, %f\n",
                 prdctx.posctx[j].log[(prdctx.posctx[j].period + i) % LOG_SIZE].voltage,

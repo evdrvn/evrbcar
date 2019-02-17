9fc5d18633f1e7d2be8f9bc10c05eced90461199
evdrvn <evdrvn@gmail.com>
2019-02-17 15:14:28 +0000

    add udp files

---
+++ src/evrbcar.h
@@ -0,0 +1,27 @@
+#ifndef __EVRBCAR_H__
+#define __EVRBCAR_H__
+
+#define EVRBCAR_UDP_PORT (65001)
+
+typedef enum evrbcar_cmd_mode{
+    EVRBCAR_CMD_STOP = 0,
+    EVRBCAR_CMD_MOVE_TO,
+    EVRBCAR_CMD_MOVE_AT,
+    EVRBCAR_CMD_LINE_TRACE,
+    EVRBCAR_CMD_TURN,
+} t_evrbcar_cmd_mode;
+
+typedef struct evrbcar_cmd_request {
+    t_evrbcar_cmd_mode mode;
+    float value[3];
+} t_evrbcar_cmd_request;
+
+typedef struct evrbcar_udp_context {
+    int sock;
+    struct sockaddr sockaddr;
+} t_evrbcar_udp_context;
+
+int evrbcar_udp_init(t_evrbcar_udp_context *udpctx, const char *address, unsigned short port);
+
+#endif
+
---
+++ src/udp.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "evrbcar.h"
+
+static void alarm_handler (int ignored) {
+    (void)ignored;
+}
+
+int evrbcar_udp_init(t_evrbcar_udp_context *udpctx, const char *address, unsigned short port) {
+    struct sockaddr_in sockaddr_in;
+    struct sigaction action;
+
+    sockaddr_in.sin_family = AF_INET;
+
+    if (inet_aton(address, &sockaddr_in.sin_addr) == 0) {
+        if (strcmp(address, "") == 0 ) {
+            sockaddr_in.sin_addr.s_addr = htonl(INADDR_ANY);
+        } else return -1;
+    }
+
+    if (port == 0) return -1;
+    sockaddr_in.sin_port = htons(port);
+
+    udpctx->sockaddr = *((struct sockaddr *)&sockaddr_in);
+
+    udpctx->sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (udpctx->sock < 0) return -1; 
+
+    action.sa_handler = alarm_handler;
+    if (sigfillset(&(action.sa_mask)) < 0) return -1;
+    action.sa_flags = 0;
+
+    if (sigaction(SIGALRM, &action, NULL) < 0) return -1;
+
+    return 0;
+}
+
+int evrbcar_udp_cmd_line_trace(t_evrbcar_udp_context *udpctx, float target_voltage){
+    int sendSize;
+    t_evrbcar_cmd_request req;
+    int size = sizeof(t_evrbcar_cmd_request);
+
+    req.mode = EVRBCAR_CMD_LINE_TRACE;
+    req.value[0] = target_voltage;
+    sendSize = sendto(udpctx->sock, &req, size, 0, &udpctx->sockaddr, sizeof(udpctx->sockaddr));
+    if (sendSize != size) return -1;
+    return sendSize;
+}
+
+
